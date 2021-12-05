edd01133dbc4d3df90d8ed7c6936b0018e3267b6
zeroZshadow <zeroZshadow@gmail.com>
2021-12-05 01:32:52 +0000

    Day 3 part 2

--- src/day03.zig
+++ src/day03.zig
@@ -1,16 +1,23 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
+const tokenize = std.mem.tokenize;
+const parseInt = std.fmt.parseInt;
+const print = std.debug.print;
+const assert = std.debug.assert;
+
 const data = @embedFile("../data/day03.txt");
 const inputType = u12;
+const inputBits = @typeInfo(inputType).Int.bits;
+const inputCounterType = std.math.Log2IntCeil(inputType);
+
+const machineType = enum {
+    Oxygen,
+    CO2,
+};
 
 pub fn main() !void {
     // Parse input
@@ -24,52 +31,83 @@ pub fn main() !void {
     }
     assert(inputList.items[0] == 2093);
 
-    // Part 1 gamma
+    // Sort the list
+    std.sort.sort(inputType, inputList.items, {}, comptime std.sort.desc(inputType));
+
+    // Part 1
     var gammaCounter: [12]usize = [_]usize{0} ** 12;
     var gammaThreshold = inputList.items.len / 2;
     for (inputList.items) |input| {
-        var i: u4 = 0;
-        while (i< @typeInfo(inputType).Int.bits) : (i+=1) {
+        var i: inputCounterType = 0;
+        while (i < inputBits) : (i += 1) {
             gammaCounter[i] += (input >> i) & 1;
         }
     }
 
     var gamma: inputType = 0;
-    var i: u4 = 0;
-    while (i < @typeInfo(inputType).Int.bits) : (i+=1) {
-        if (gammaCounter[i] >= gammaThreshold){
+    var i: inputCounterType = 0;
+    while (i < inputBits) : (i += 1) {
+        if (gammaCounter[i] >= gammaThreshold) {
             gamma = gamma | (@as(inputType, 1) << i);
         }
     }
     assert(gamma == 1337);
     const result = @as(u32, gamma) * @as(u32, ~gamma);
-    print("Gamma {} Epsilon {} Result {}\n", .{gamma, ~gamma, result});
+    print("Gamma {} Epsilon {} Result {}\n", .{ gamma, ~gamma, result });
+
+    // Part 2
+    var oxygenOutput = findValue(inputList.items, .Oxygen);
+    assert(oxygenOutput == 1599);
+    print("Oxygen {}\n", .{oxygenOutput});
+
+    var co2Output = findValue(inputList.items, .CO2);
+    assert(co2Output == 2756);
+    print("CO2 {}\n", .{co2Output});
+
+    var lifeSupport = @as(u32, oxygenOutput) * @as(u32, co2Output);
+    print("Life Support {}\n", .{lifeSupport});
+    assert(lifeSupport == 4406844);
 }
 
-// Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+fn findInputSplit(input: []inputType, bit: inputCounterType) ?usize {
+    var mask = (@as(inputType, 1) << bit);
+    for (input) |item, index| {
+        if ((item & mask) != mask) {
+            return index;
+        }
+    }
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+    return null;
+}
 
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
+fn findValue(input: []inputType, machine: machineType) inputType {
+    var i: inputCounterType = inputBits;
+    var items: []inputType = input[0..];
+    while (i > 0) {
+        i -= 1;
+        if (items.len == 1) {
+            return items[0];
+        }
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+        var threshold = (items.len + 1) / 2;
+        var index = findInputSplit(items, i) orelse continue;
 
-const sort = std.sort.sort;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+        if (index >= threshold) {
+            if (machine == .Oxygen) {
+                items = items[0..index];
+            } else {
+                items = items[index..];
+            }
+        } else {
+            if (machine == .Oxygen) {
+                items = items[index..];
+            } else {
+                items = items[0..index];
+            }
+        }
+    }
+
+    assert(items.len == 1);
+
+    return items[0];
+}
